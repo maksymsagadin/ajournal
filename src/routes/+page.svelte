5c5b2907225118d043a6af8a93a3400f5cd7df1a
Maksym Sagadin <maxsagadin@gmail.com>
2023-05-27 03:24:20 +0000

    Develop HTML structure.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import JournalEntry from "../lib/journal-entry.svelte"
+    const title = 'Journal'
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<form action='' method=''>
+    <input type='text' name='text' aria-label='Add a journal entry' placeholder='Add a journal entry'>
+</form>
+
+<JournalEntry />
+<JournalEntry />
