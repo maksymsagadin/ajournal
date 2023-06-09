c87f0774d26ba3936ee4e0e41c033302d0fc7974
Maksym Sagadin <maxsagadin@gmail.com>
2023-06-09 01:45:01 +0000

    Use @prisma/client.

--- src/routes/api/journal-entries/+server.ts
+++ src/routes/api/journal-entries/+server.ts
@@ -8,7 +8,6 @@ export const GET: RequestHandler = ({ request }) => {
 export const POST: RequestHandler = async ({ request }) => {
     const {text} = await request.json()
     return api(request, {
-        uid: `${Date.now()}`, //JournalEntries: replace with UID from database
         created_at: new Date(),
         text: text,
         done: false
